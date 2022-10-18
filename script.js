f727ee530275d2605badad1cf5cd42e893b4887a
James A Williams <williamsjamesalan65@gmail.com>
2022-10-17 21:01:09 -0500

    finished project

--- script.js
+++ script.js
@@ -32,19 +32,24 @@ function generatePassword() {
   // WHEN all prompts are answered
   // THEN a password is generated that matches the selected criteria
   // WHEN the password is generated
-  // THEN the password is either displayed in an alert or written to the page
-  const alpha = "abcdefghijklmnopqrstuvwxyz";
-  const Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const numbers = "0123456789";
-  const symbols = "!@#$%^&*_-+=|\?/><{}";
+  // THEN the password is either displayed in an alert or written to the page"t",
+  //seperated Capital, lower case, numeric, and special symbols
+
+  const alpha = ["a", "b", "c",  "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",  "w", "x", "y", "z", ];
+  const calpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+  const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
+  const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "|",];
 
   const passwordTxt = document.getElementById("password");
   const length = document.getElementById("length");
   const incNumbers = document.getElementById("numbers");
   const incSymbols = document.getElementById("symbols");
   const generateBtn = document.getElementById("generate");
-
-  return "make sure to replace this with the finalized random password";
+  var password = ""
+  for(var i = 0; i< userChoice; i++) {
+    password += generate(lowercase, uppercase, numeric, special_characters, alpha, calpha, numbers, symbols)
+  }
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
@@ -56,8 +61,32 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-let characters = alpha;
-  incNumbers.checked ? (characters += numbers) : "";
-  incSymbols.checked ? (characters += symbols) : "";
-  passwordTxt.value = generatePassword(length.value, characters);
+
+// comment out to see if it workslet characters = alpha; 
+ // comment out to see if it works incNumbers.checked ? (characters += numbers) : "";
+ // comment out to see if it works incSymbols.checked ? (characters += symbols) : "";
+ // comment out to see if it works passwordTxt.value = generatePassword(length.value, characters);
 /* Deleted line*/
+function generate(lowercase, uppercase, numeric, special_characters, alpha, calpha, numbers, symbols) 
+{
+  var arrays = []
+ if (lowercase === true)
+ arrays.push (alpha)
+ if (uppercase === true)
+ arrays.push (calpha)
+ if (numeric === true)
+ arrays.push (numbers)
+ if (special_characters === true)
+ arrays.push (symbols)
+var selectedindex = Math.floor(Math.random()*arrays.length)
+
+var selectedarray = arrays[selectedindex]
+
+var selectedcharacter = Math.floor(Math.random()*selectedarray.length)
+console.log (selectedcharacter)
+var returning = selectedarray[
+  selectedcharacter 
+]
+return returning
+}
+
